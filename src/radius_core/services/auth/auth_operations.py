5b55ef98bc26433df8fe2958ed2a6096eff6973d
LEO <krokxa228@gmail.com>
2025-09-22 17:05:09 +0500

    add getattr

--- src/radius_core/services/auth/auth_operations.py
+++ src/radius_core/services/auth/auth_operations.py
@@ -363,9 +363,9 @@ async def _save_auth_log(
             )
 
         log_entry = AuthDataLog(
-            login=login.login if login.login else None,
+            login=getattr(login, "login", None) if login else None,
             username=data.User_Name,
-            password=login.password if login.password else None,
+            password=getattr(login, "password", None) if login else None,
             callingstationid=data.Calling_Station_Id,
             nasipaddress=data.NAS_IP_Address,
             reply=reply_code,
